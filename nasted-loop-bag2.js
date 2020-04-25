55d8db23d264157ec510cc540b8e5eaace64eb11
irfan-fauzi <irfanwintech@yahoo.com>
2020-04-25 20:28:41 +0700

    segitiga angka berhasil

---
+++ nasted-loop-bag2.js
@@ -0,0 +1,69 @@
+let noAngkot = 1;
+let jmlAngkot = 10;
+
+
+for (noAngkot; noAngkot <= jmlAngkot; noAngkot++) {
+    if (noAngkot <= 6 && noAngkot != 1) {
+        console.log(`angkot no ${noAngkot} sedang beroperasi`);
+    } else if (noAngkot == 8 || noAngkot == 10 || noAngkot == 1) {
+        console.log(`angkot no ${noAngkot} sedang lembur`);
+    } else {
+        console.log(`angkot no ${noAngkot} sedang tidak beroperasi`);
+    }
+
+}
+
+// segitiga 1
+for (let a = 1; a <= 10; a++) {
+    for (b = 1; b <= a; b++) {
+        document.write(b);
+    }
+    document.write(`<br>`)
+}
+document.write(`<br>`);
+
+// segitiga 2
+for (let a = 1; a <= 10; a++) {
+    for (let b = a; b <= 10; b++) {
+        document.write(b);
+    }
+    document.write(`<br>`);
+}
+document.write(`<br>`);
+
+// segitiga 3 <piramida
+for (let a = 1; a <= 10; a++) {
+    for (let b = a; b <= 10; b++) {
+        document.write(`*`);
+    }
+    for (let c = 1; c <= a; c++) {
+        document.write(c);
+    }
+    for (let d = 1; d <= a - 1; d++) {
+        document.write(d);
+    }
+    for (let e = a; e <= 10; e++) {
+        document.write(`*`);
+    }
+
+
+    document.write(`<br>`);
+}
+document.write(`<br>`);
+
+// segitiga 4 piramida 
+for (let a = 1; a <= 10; a++) {
+    for (let b = a; b <= 10; b++) {
+        document.write(`*`);
+    }
+    for (let c = 1; c <= a; c++) {
+        document.write(c);
+    }
+    for (let d = 1; d <= a - 1; d++) {
+        document.write(d);
+    }
+    for (let e = a; e <= 10; e++) {
+        document.write(`*`);
+    }
+    document.write(`<br>`);
+}
--- nasted-loop.js
+++ nasted-loop.js
@@ -22,13 +22,100 @@
 
 // latihan 2
 
-for (let a = 1; a <= 3; a++) {
-    // selama b = true , maka pengulangan b akan terus berlanjut sampai b = false
-    // jika b = false, maka a ++ baru dijalankan
-    for (let b = 1; b <= 3; b++) {
-        document.write(`${b}`);
+// for (let a = 1; a <= 3; a++) {
+//     // selama b = true , maka pengulangan b akan terus berlanjut sampai b = false
+//     // jika b = false, maka a ++ baru dijalankan
+//     for (let b = 1; b <= 3; b++) {
+//         document.write(`${b}`);
+//     }
+
+//     document.write(` <br> `);
+
+// }
+
+// latihan 3
+// for (let a = 1; a <= 5; a++) {
+
+//     for (let b = 1; b <= a; b++) {
+//         document.write(`${b}b`);
+//     }
+
+//     document.write(`<br>`);
+
+// }
+
+// latihan 4
+// jika kita menulis variable let b diluar {} maka b tidak dapat di akses
+
+for (let a = 1; a <= 5; a++) {
+
+    for (let b = 1; b <= a; b++) {
+        document.write(`${b}b`);
+    }
+    for (let c = a; c <= 5; c++) {
+        document.write(`${c}c`);
     }
 
-    document.write(` <br> `);
+    document.write(`<br>`);
 
+}
+document.write(`<br>`);
+for (let a = 1; a <= 5; a++) {
+
+    for (let b = 1; b <= a; b++) {
+        document.write(`${b}b`);
+    }
+    // for (let c = a; c <= 5; c++) {
+    //     document.write(`${c}c`);
+    // }
+
+    document.write(`<br>`);
+
+}
+document.write(`<br>`)
+
+for (let a = 6; a >= 1; a--) {
+    for (let b = a; b >= 1; b--) {
+        document.write(b)
+    }
+    document.write(`<br>`);
+}
+document.write(`<br>`);
+
+for (let a = 5; a >= 1; a--) {
+    for (let b = a; b >= 1; b--) {
+        document.write(`_`);
+    }
+    for (let c = a; c <= 5; c++) {
+        document.write(`c`);
+    }
+    document.write(`<br>`);
+}
+
+document.write(`<br>`);
+
+for (let a = 5; a >= 1; a--) {
+    for (let b = a; b >= 1; b--) {
+        document.write(`_`);
+    }
+    for (let c = a; c <= 5; c++) {
+        document.write(`c`);
+    }
+    for (let d = a; d <= 5; d++) {
+        document.write(`d`);
+    }
+    for (let e = a; e >= 1; e--) {
+        document.write(`_`);
+    }
+
+    document.write(`<br>`);
+}
+
+document.write(`<br>`);
+
+for (let a = 1; a <= 10; a++) {
+    for (let b = a; b <= 10; b++) {
+        document.write(`${b}`);
+    }
+    document.write(`<br>`);
 }
