856c45266e7a03e4c23d65ba077634763c6771bd
irfan-fauzi <irfanwintech@yahoo.com>
2020-04-26 21:24:06 +0700

    tambahkan pengulangan di suwit jawa

--- suwit-jawa.js
+++ suwit-jawa.js
@@ -1,45 +1,50 @@
 // menangkap pilihan player
-let player = prompt(`pilih : gajah, orang, semut`);
+let tanya = true;
+// ketika true jalankan kode dibawah
+// sampai confirm = false
+while (tanya) {
+    let player = prompt(`pilih : gajah, orang, semut`);
+
+    // tangkap pilihan computer
+    let comp = Math.random();
+
+    if (comp <= 0.34) {
+        comp = `semut`;
+    } else if (comp >= 0.34 && comp < 0.67) {
+        comp = `orang`;
+    } else {
+        comp = `gajah`;
+    }
 
 
-let comp = Math.random();
+    // rules -----------------
+    let hasil = ``;
+    if (comp == player) {
+        hasil = `seri`;
+        // jika playr = gajah
+    } else if (player == 'gajah') {
+        if (comp == 'orang') {
+            hasil = `menang`;
+        } else {
+            hasil = `kalah`;
+        }
+    } else if (player == `orang`) {
+        if (comp == 'gajah') {
+            hasil = `kalah`;
+        } else {
+            hasil = `menang`;
+        }
+    } else if (player == `semut`) {
+        if (comp == 'orang') {
+            hasil = `kalah`;
+        } else {
+            hasil = `menang`;
+        }
 
+    } else {
+        hasil = `eror`;
+    }
 
-if (comp <= 0.34) {
-    comp = `semut`;
-} else if (comp >= 0.34 && comp < 0.67) {
-    comp = `orang`;
-} else {
-    comp = `gajah`;
+    alert(`kamu memilih : ${player}, dan komputer memilih ${comp} \nHasilnya kamu : ${hasil}`);
+    tanya = confirm(`lakukan lagi ?`);
 }
-
-
-
-let hasil = ``;
-if (comp == player) {
-    hasil = `seri`;
-    // jika playr = gajah
-} else if (player == 'gajah') {
-    if (comp == 'orang') {
-        hasil = `menang`;
-    } else {
-        hasil = `kalah`;
-    }
-} else if (player == `orang`) {
-    if (comp == 'gajah') {
-        hasil = `you lose`;
-    } else {
-        hasil = `player lose `;
-    }
-} else if (player == `semut`) {
-    if (comp == 'orang') {
-        hasil = `you lose`;
-    } else {
-        hasil = `you win `;
-    }
-
-} else {
-    hasil = `eror`;
-}
-
-alert(`kami memilih : ${player}, dan komputer memilih ${comp} \nHasilnya kamu : ${hasil}`);
