95bd3621f86365cf432a5230ac796f97fa105fee
irfan-fauzi <irfanwintech@yahoo.com>
2020-04-26 21:13:31 +0700

    papancatur belum

---
+++ papan-catur.js
@@ -0,0 +1,11 @@
+let sharp = `#`;
+let star = `*`;
+let hasil = "";
+let num = 8;
+
+for (let a = 1; a <= 8; a++) {
+    for (let b = 1; b <= 8; b++) {
+        document.write(sharp)
+    }
+    document.write(`<br>`)
+}
