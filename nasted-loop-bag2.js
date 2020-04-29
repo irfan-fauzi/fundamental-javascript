08cf2fc4b1eeeb292b2a3c04307808fa8201df29
irfan-fauzi <irfanwintech@yahoo.com>
2020-04-29 22:58:35 +0700

    papan catur penyempurnaan

--- nasted-loop-bag2.js
+++ nasted-loop-bag2.js
@@ -1,17 +1,17 @@
-let noAngkot = 1;
-let jmlAngkot = 10;
+// let noAngkot = 1;
+// let jmlAngkot = 10;
 
 
-for (noAngkot; noAngkot <= jmlAngkot; noAngkot++) {
-    if (noAngkot <= 6 && noAngkot != 1) {
-        console.log(`angkot no ${noAngkot} sedang beroperasi`);
-    } else if (noAngkot == 8 || noAngkot == 10 || noAngkot == 1) {
-        console.log(`angkot no ${noAngkot} sedang lembur`);
-    } else {
-        console.log(`angkot no ${noAngkot} sedang tidak beroperasi`);
-    }
+// for (noAngkot; noAngkot <= jmlAngkot; noAngkot++) {
+//     if (noAngkot <= 6 && noAngkot != 1) {
+//         console.log(`angkot no ${noAngkot} sedang beroperasi`);
+//     } else if (noAngkot == 8 || noAngkot == 10 || noAngkot == 1) {
+//         console.log(`angkot no ${noAngkot} sedang lembur`);
+//     } else {
+//         console.log(`angkot no ${noAngkot} sedang tidak beroperasi`);
+//     }
 
-}
+// }
 
 // segitiga 1
 for (let a = 1; a <= 10; a++) {
@@ -30,6 +30,16 @@ for (let a = 1; a <= 10; a++) {
     document.write(`<br>`);
 }
 document.write(`<br>`);
+
+// segitiga 2 versi 2
+for (let a = 10; a >= 1; a--) {
+    for (let b = 1; b <= a; b++) {
+        document.write(b);
+    }
+    document.write(`<br>`);
+}
+document.write(`<br>`)
+
 
 // segitiga 3 <piramida
 for (let a = 1; a <= 10; a++) {
@@ -84,7 +94,7 @@ for (a = 1; a <= 10; a++) {
 }
 document.write(`<br>`);
 
-// papan catur
+// papan catur versi hardcore
 for (let i = 1; i <= 5; i++) {
     for (let b = 1; b <= 20; b++) {
         if (b % 2 == 0) {
@@ -99,6 +109,57 @@ for (let i = 1; i <= 5; i++) {
             document.write(`*`);
         } else {
             document.write(`#`);
+        }
+    }
+    document.write(`<br>`);
+
+}
+document.write(`<br>`);
+// versi 2
+
+let pagar = `#`;
+let bintang = "*";
+let vertikal = 15;
+let horizontal = 10;
+
+// for (let a = 1; a <= nomor; a++) { // buat vertikal
+//     for (let b = 1; b <= nomor; b++) { // horizontal
+//         // jika ganjil
+//         if (a % 2 == 1) {
+
+//             if (b % 2 == 0) {
+//                 hasil += pagar;
+//             } else {
+//                 hasil += bintang;
+//             }
+//         } else {
+//             if (b % 2 == 0) {
+//                 hasil += bintang;
+//             } else {
+//                 hasil += pagar;
+//             }
+//         }
+//     }
+//     hasil = hasil + `\n`;
+//     console.log(hasil)
+//     hasil = ``;
+
+for (let a = 1; a <= vertikal; a++) { // buat vertikal
+    for (let b = 1; b <= horizontal; b++) { // horizontal
+        // jika ganjil
+        if (a % 2 == 1) {
+
+            if (b % 2 == 0) {
+                document.write(`${pagar}`);
+            } else {
+                document.write(bintang);
+            }
+        } else {
+            if (b % 2 == 0) {
+                document.write(bintang);
+            } else {
+                document.write(pagar)
+            }
         }
     }
     document.write(`<br>`);
