e3952b4e7b55340d9172422d62346dec5c63a7a2
irfan-fauzi <irfanwintech@yahoo.com>
2020-05-02 00:51:33 +0700

    memecah-mecah masalah menjadi beberapa fn

--- function.js
+++ function.js
@@ -78,3 +78,71 @@ let segitigaAngka = function (jmlAngka) {
 }
 
 // membuat game suwit dengan function
+
+// komputer membuat pilihan dengan random
+// --------------------------- INPUT ----------------------------
+let comp = Math.random();
+let player = `gajah`;
+// --------------------------- / INPUT ----------------------------
+
+// --------------------------- PROSES ---------------------------
+// memproses pilihan komputer menghasilkan output
+let pilihanComputer = function (comp) {
+    let hasil = ``;
+    if (comp <= 0.34) {
+        hasil = `semut`;
+    } else if (comp >= 0.34 && comp < 0.67) {
+        hasil = `orang`;
+    } else {
+        hasil = `gajah`;
+    }
+    return hasil;
+}
+
+// --------------------------- PROSES ---------------------------
+let pilihanPlayer = function (player, kom) {
+    let hasil = ``;
+    if (player == kom) {
+        hasil = `seri`;
+    } else if (player == `gajah`) {
+        if (kom == `orang`) {
+            hasil = `player menang komputer memilih ${kom}`;
+        } else {
+            hasil = `player kalah komputer memilih ${kom}`;
+        }
+    } else if (player == `orang`) {
+        if (kom == `gajah`) {
+            hasil = `player kalah komputer memilih ${kom}`
+        } else {
+            hasil = `player menang komputer memilih ${kom}`;
+        }
+    } else if (player == `semut`) {
+        if (kom == `gajah`) {
+            hasil = `player menang komputer memilih ${kom}`;
+        } else {
+            hasil = `player kalah komputer memilih ${kom}`;
+        }
+    } else { // jika player input karakter selain 3
+        hasil = `salah input`;
+    }
+    return hasil;
+}
+// -------------------- / PROSES ---------------------------
+
+// --------------------  OUTPUT -----------------------
+// fungsi pilihanComputer masukan ke variabel
+let pilKomputer = pilihanComputer(comp);
+let pilPlayer = pilihanPlayer(player, pilKomputer);
+
+
+
+
+
+
+
+
+
+
+
+
+// -------------------  / OUTPUT
