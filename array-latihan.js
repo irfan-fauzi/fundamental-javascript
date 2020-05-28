f398f7eccd6fa52fcfcf677aabdf88f8e25d19af
irfan-fauzi <irfanwintech@yahoo.com>
2020-05-28 19:21:44 +0700

    latihan 0bject for

--- array-latihan.js
+++ array-latihan.js
@@ -45,11 +45,6 @@ let hapusPenumpang = function (penumpang, array) {
     }
 }
 
-let num = [1, 2, 3, 4, 5];
-for (let i = 0; i < num.length; i++) {
-    console.log(i);
-}
-
 
 
 button.addEventListener('click', function () {
--- latihan-object.js
+++ latihan-object.js
@@ -23,16 +23,21 @@ const Angkot = function (sopir, trayek) {
         return this.penumpang;
     }
     this.penumpangTurun = function (penumpang) {
-        // this.penumpang.pop(penumpang);
-        // this.kas += 5000;
-        // return this.penumpang;
+        if (this.penumpang.length == 0) {
+            alert("penumpang kosong");
+            return this.penumpang;
+        }
         for (let i = 0; i < this.penumpang.length; i++) {
             if (this.penumpang[i] == penumpang) {
                 this.penumpang[i] = undefined;
                 this.kas += 100;
+                return this.penumpang;
+            } else if (i == this.penumpang.length - 1) {
+                alert("nama penumpang tidak ada");
                 return this.penumpang;
             }
         }
     }
 }
 let angkot1 = new Angkot(`udin`, `majalengka - kadipaten`);
+let angkot2 = new Angkot(`supardi`, `majalengka - kadipaten`);
